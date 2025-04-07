309a2a70b6e892e9dd6321c2eb4e995e85d75575
kennykaye-doordash <kenny.kaye@doordash.com>
2025-04-07 00:17:50 -0400

    Better Wezterm borders

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -73,41 +73,15 @@ config.term = "xterm-256color"
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 config.show_tab_index_in_tab_bar = true
-config.tab_max_width = 50
+config.tab_max_width = 100
 config.use_fancy_tab_bar = true
+config.window_decorations = 'RESIZE'
+config.window_padding = { left = '0.5cell', right = '0.5cell', top = '0.5cell', bottom = '0cell' }
+
 
 -- Theme configuration
 
--- -- Explicitly force colors (Base16 Eighties)
 config.colors = {
---   -- Default colors
---   foreground = "#d3d0c8",
---   background = "#2d2d2d",
-
---   -- Normal colors (ANSI 0-7)
---   ansi = {
---     "#2d2d2d", -- black 
---     "#f2777a", -- red
---     "#99cc99", -- green
---     "#ffcc66", -- yellow
---     "#6699cc", -- blue
---     "#cc99cc", -- magenta
---     "#66cccc", -- cyan
---     "#d3d0c8", -- white
---   },
-
---   -- Bright colors (ANSI 8-15)
---   brights = {
---     "#747369", -- bright black
---     "#f2777a", -- bright red
---     "#99cc99", -- bright green
---     "#ffcc66", -- bright yellow
---     "#6699cc", -- bright blue
---     "#cc99cc", -- bright magenta
---     "#66cccc", -- bright cyan
---     "#f2f0ec", -- bright white
---   },
-
 --   -- Custom cursor colors
 --   cursor_bg = "#f2777a",
 --   cursor_fg = "#000000", -- Black cursor text
@@ -122,29 +96,29 @@ config.colors = {
   -- to this color to give a visual cue about the compose state.
   compose_cursor = 'orange', 
     -- Tab bar colors
---   tab_bar = {
---     background = "#2d2d2d",
---     active_tab = {
---       bg_color = "#2d2d2d",
---       fg_color = "#f2777a",
---     },
---     inactive_tab = {
---       bg_color = "#2d2d2d",
---       fg_color = "#747369",
---     },
---     inactive_tab_hover = {
---       bg_color = "#2d2d2d",
---       fg_color = "#d3d0c8",
---     },
---     new_tab = {
---       bg_color = "#2d2d2d",
---       fg_color = "#d3d0c8",
---     },
---     new_tab_hover = {
---       bg_color = "#2d2d2d",
---       fg_color = "#f2777a",
---     },
---   },
+  tab_bar = {
+    background = "#2d2d2d",
+    active_tab = {
+      bg_color = "#2d2d2d",
+      fg_color = "#ffffff",
+    },
+    inactive_tab = {
+      bg_color = "#2d2d2d",
+      fg_color = "#747369",
+    },
+    inactive_tab_hover = {
+      bg_color = "#2d2d2d",
+      fg_color = "#d3d0c8",
+    },
+    new_tab = {
+      bg_color = "#2d2d2d",
+      fg_color = "#d3d0c8",
+    },
+    new_tab_hover = {
+      bg_color = "#2d2d2d",
+      fg_color = "#f2777a",
+    },
+  },
 }
 
 -- Cursor configuration
